5723bff86cecfc0534e43df9c36e518adc55bc02
sarvesh-astrophile <sarvesh9301@gmail.com>
2024-07-22 18:13:06 +0530

    sol 2

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware, clerkMiddleware } from "@clerk/nextjs/server";
 
 export default authMiddleware({
-  publicRoutes: ["/api/webhooks/clerk"],
+  ignoredRoutes: ["/api/webhooks(.*)"],
 });
 
 export const config = {
